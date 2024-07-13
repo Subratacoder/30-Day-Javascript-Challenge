58d1fb6cb910e39de367ced489c89018ae6154b4
Subrata Acharjee <rp6398812@gmail.com>
2024-07-13 12:48:26 +0100

    Upload files

---
+++ 01_challange.js
@@ -0,0 +1,44 @@
+//Activity-1 Variable Declearation
+var num = 28;
+let name = "Subrata";
+
+// console.log(num);
+// console.log(name);
+
+//Activity-2 Constant Declearation
+const  userLoggedIn = 'false';
+// console.log(userLoggedIn);
+
+//Activity-3 Data Types
+const num1 = 40;
+const str = "This is string";
+const bool = "true"
+const userDetail = {
+    name: "Subrata",
+    age: 20,
+    address: "kolkata",
+}
+const myFavHero = ['Ironman', 'Superman', 'Batman', 'Thor']
+
+// console.log(typeof num1);
+// console.log(typeof str);
+// console.log(typeof bool);
+// console.log(typeof userDetail);
+// console.log(typeof myFavHero);
+
+//Activity-4 Reassigning Variables
+let place = 'Kashmir';
+// console.log(place);
+place = 'Thailand';
+// console.log(place);
+
+//Activity-4 Understanding Const
+const favCar = 'BMW';
+// console.log(
+// favCar = 'Mustang';
+
+console.log(`value = ${num1}, Type: ${typeof num1}`);
+console.log(`value = ${str}, Type: ${typeof str}`);
+console.log(`value = ${bool}, Type: ${typeof bool}`);
+console.log(`value = ${userDetail}, Type: ${typeof userDetail}`);
+console.log(`value = ${myFavHero}, Type: ${typeof myFavHero}`);
